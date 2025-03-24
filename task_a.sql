82141122a3a55ef1cc8e50513ae6e410da95f7ee
yguo <unclebnn199@gmail.com>
2025-03-24 12:49:37 -0700

    first commit

--- task_a.sql
+++ task_a.sql
@@ -2,8 +2,12 @@
 -- volunteer registration: 1. insert into users table first(since volunteers must be users)
 -- 2. Insert into volunteers table using the new user's ID
 
-INSERT INTO users (firstName,lastName,email,password,zipCode,role,neighborhood)
-VALUES ('John','Doe','john.doe@email.com','securepass123','94143','volunteer','Rockridge');
+INSERT INTO users (firstName, lastName, email, password, zipCode, neighborhood)
+VALUES ('John', 'Doe', 'john.doe@email.com', 'securepass123', '94143', 'Rockridge');
 SET @new_user_id = LAST_INSERT_ID(); -- Get the ID of the newly inserted user
-INSERT INTO volunteers (vid,applicationStatus,availability)
-VALUES (@new_user_id,'submitted',true);
+INSERT INTO volunteers (vid, applicationStatus, availability)
+VALUES (@new_user_id, 'submitted', true);
+
+-- resident registration: just insert into users table
+INSERT INTO users (firstName, lastName, email, password, zipCode, neighborhood)
+VALUES ('Jane', 'Doe', 'jane.doe@email.com', 'securepass345', '94140', 'Temescal');

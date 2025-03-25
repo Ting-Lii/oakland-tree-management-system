642740f3ac5bf4c43a6f6f4eaed6cc06b7e7efed
yguo <unclebnn199@gmail.com>
2025-03-24 22:41:36 -0700

    first commit

--- task_j.sql
+++ task_j.sql
@@ -1,7 +1,33 @@
+-- For every species of trees, find the number of trees planted and:
+-- the number of years since the first tree of the species was planted, the number of years since the most recent tree of the species was planted.
 SELECT
-    treePlanted AS tree_name,
+    treePlanted AS species,
     COUNT(*) AS amount,
-    EXTRACT(YEAR FROM CURRENT_DATE) - MIN(EXTRACT(YEAR FROM plantDate)) AS years_since_first_planting
+    EXTRACT(YEAR FROM CURRENT_DATE) - MIN(EXTRACT(YEAR FROM plantDate)) AS years_since_first_planting,
+    EXTRACT(YEAR FROM CURRENT_DATE) - MAX(EXTRACT(YEAR FROM plantDate)) AS years_since_last_planting
 FROM treePlantings
-GROUP BY treePlanted
-ORDER BY amount DESC, tree_name;
+GROUP BY species
+ORDER BY amount DESC, species;
+
+-- the year that had the most trees of the species planted and the number of trees planted.
+SELECT t.treePlanted AS species, COUNT(*) AS total_planted,
+    (
+        SELECT EXTRACT(YEAR FROM plantDate)
+        FROM treePlantings t_sub
+        WHERE t_sub.treePlanted = t.treePlanted
+        GROUP BY EXTRACT(YEAR FROM plantDate)
+        ORDER BY COUNT(*) DESC
+        LIMIT 1
+    ) AS year_with_most,
+    (
+        SELECT COUNT(*)
+        FROM treePlantings t_sub2
+        WHERE t_sub2.treePlanted = t.treePlanted
+        GROUP BY EXTRACT(YEAR FROM plantDate) -- aggregates all rows that occurred within the same year
+        ORDER BY COUNT(*) DESC
+        LIMIT 1
+    ) AS num_planted_in_year
+FROM treePlantings t
+GROUP BY t.treePlanted;
+
+

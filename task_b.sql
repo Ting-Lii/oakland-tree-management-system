f0c769ee7becd2604b5de85233ad87bb0b43f6d7
yguo <unclebnn199@gmail.com>
2025-03-25 08:40:04 -0700

    use stored procedure process_tree_request

--- task_b.sql
+++ task_b.sql
@@ -1,56 +1,77 @@
--- Accept a request for a tree, registering as a new user if necessary.
--- When requesting a tree, users will need to specify their address
+-- Accept a request for a tree, registering as a new user if necessary using stored procedure from task a
+-- Create the tree request using the parameterized values
+
+-- stored procedure to process a tree request
+DELIMITER //
+CREATE PROCEDURE process_tree_request(
+    IN p_user_email VARCHAR(40),
+    IN p_firstName VARCHAR(40),
+    IN p_lastName VARCHAR(40),
+    IN p_password VARCHAR(40),
+    IN p_zipCode VARCHAR(10),
+    IN p_role VARCHAR(20),
+    IN p_neighborhood VARCHAR(60),
+    IN p_streetAddress VARCHAR(100),
+    IN p_requestZip VARCHAR(10),
+    IN p_phone VARCHAR(20),
+    IN p_amount DECIMAL(10,2),
+    IN p_relationship VARCHAR(20),
+    IN p_requestStatus VARCHAR(20)
+)
+BEGIN
+    DECLARE existing_user_id INT;
+    DECLARE new_user_id INT;
 
 -- First, check if user exists by email
-SET @user_email = 'sarasmith@email.com';
-SET @existing_user_id =
-    (SELECT uid
+    SELECT uid INTO existing_user_id
     FROM users
-    WHERE email = @user_email);
+    WHERE email = p_user_email;
 
--- If user doesn't exist, create new user
-IF @existing_user_id IS NULL THEN
-    INSERT INTO users (firstName, lastName, email, password, zipCode, role, neighborhood)
+    -- If user doesn't exist, use stored procedure register_user from task a
+    IF existing_user_id IS NULL THEN
+        CALL register_user(
+                p_firstName,
+                p_lastName,
+                p_user_email,
+                p_password,
+                p_zipCode,
+                p_role,
+                p_neighborhood,
+                false, -- not a volunteer
+                new_user_id
+             );
+            SET existing_user_id = new_user_id;
+    END IF;
+
+    -- Generate a unique reference number for the tree request
+    SET @ref_num = UUID();
+
+    -- Create the tree request using parameterized values
+    INSERT INTO treeRequests (
+        referenceNum,
+        rid,
+        streetAddress,
+        zipCode,
+        phone,
+        amountOfPayment,
+        relationshipToProperty,
+        dateSubmitted,
+        requestStatus,
+        neighborhood
+    )
     VALUES (
-        'Sarah',
-        'Smith',
-        @user_email,
-        'securepass789',
-        '94611',
-        'resident',
-        'Clawson'
-    );
-    SET @existing_user_id = LAST_INSERT_ID();
-END IF;
+               @ref_num,
+               existing_user_id,
+               p_streetAddress,
+               p_requestZip,
+               p_phone,
+               p_amount,
+               p_relationship,
+               CURRENT_DATE,
+               p_requestStatus,
+               p_neighborhood);
 
--- Generate a unique reference number for the tree request
-SET @ref_num = UUID();
-
--- Create the tree request
-INSERT INTO treeRequests (
-    referenceNum,
-    rid,
-    streetAddress,
-    zipCode,
-    phone,
-    amountOfPayment,
-    relationshipToProperty,
-    dateSubmitted,
-    requestStatus,
-    neighborhood
-)
-VALUES (
-           @ref_num,
-           @existing_user_id,
-           '123 Oak Street',
-           '94611',
-           '(510)555-0123',
-           0.00,
-           'owner',
-           CURRENT_DATE,
-           'submitted',
-           'Clawson'
-       );
-
--- Return the reference number for the request
-SELECT @ref_num AS 'Tree Request Reference Number';
+    -- Return the reference number for the request
+    SELECT @ref_num AS 'Tree Request Reference Number';
+END //
+DELIMITER ;

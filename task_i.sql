58769c992f35ec972552d3b3802cb1a8e28b886e
Cathenax <siyuan20011205@163.com>
2025-04-21 21:43:20 -0700

    removed all unnecessary files

--- task_i.sql
+++
@@ -1,6 +0,0 @@
-SELECT tp.plantDate, tp.streetAddress, tp.zipCode, tr.neighborhood, tp.treePlanted
-FROM treePlantings tp
-JOIN treeRequests tr ON tp.requestRefNum = tr.referenceNum
-WHERE tr.neighborhood IN ('Acron', 'Santa Fe', 'Oak Center', 'Bushrod')
-   OR tp.zipCode IN ('93065', '59359')
-ORDER BY tp.plantDate;
